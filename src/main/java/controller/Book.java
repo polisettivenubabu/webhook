df56fc5f08cc981cf7bb2f4c39e1865458f0ca2d
venu babu <vpolishetti@nextsphere.com>
2021-09-27 11:19:06 +0530

    sample

---
+++ src/main/java/controller/Book.java
@@ -0,0 +1,17 @@
+package controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/webhook")
+public class Book {
+
+    @PostMapping
+    public ResponseEntity<String> print(@RequestBody String rbody){
+        System.out.println("sample"+ rbody);
+        return new ResponseEntity<String> (rbody, HttpStatus.OK);
+    }
+
+}
