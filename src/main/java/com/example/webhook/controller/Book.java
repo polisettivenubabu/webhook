c9ca247a17a07c26d687d4c7d711e21c3f549f64
venu babu <vpolishetti@nextsphere.com>
2021-09-27 16:29:40 +0530

    changes

--- src/main/java/com/example/webhook/controller/Book.java
+++ src/main/java/com/example/webhook/controller/Book.java
@@ -1,18 +1,20 @@
-package controller;
+package com.example.webhook.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/webhook")
 public class Book {
 
-    @PostMapping
+    @PostMapping(value="/webhook")
     public ResponseEntity<String> print(@RequestBody String rbody){
         System.out.println("sample"+ rbody);
 
         return new ResponseEntity<String> (rbody, HttpStatus.OK);
     }
-
+    @GetMapping("/get")
+    public void display(){
+        System.out.println("sample");
+           }
 }
