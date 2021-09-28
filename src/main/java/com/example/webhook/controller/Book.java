9b0b26f569f20896b6004eeb6f4cb507f85ba3aa
venu babu <vpolishetti@nextsphere.com>
2021-09-28 11:00:13 +0530

    updated

--- src/main/java/com/example/webhook/controller/Book.java
+++ src/main/java/com/example/webhook/controller/Book.java
@@ -7,14 +7,26 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class Book {
 
-    @PostMapping(value="/webhook")
-    public ResponseEntity<String> print(@RequestBody String rbody){
-        System.out.println("sample"+ rbody);
+    @PostMapping(value="/post")
+    public ResponseEntity<String> Creatd(@RequestBody String rbody){
 
-        return new ResponseEntity<String> (rbody, HttpStatus.OK);
+        System.out.println("Created Successfully.. "+rbody);
+        return new ResponseEntity<String>(rbody,HttpStatus.OK);
     }
     @GetMapping("/get")
-    public void display(){
-        System.out.println("sample");
-           }
+    public String display(){
+
+        System.out.println("Retrived Successfully.. ");
+        return "Retrived Successfully.. ";
+    }
+     @PutMapping("/put")
+    public String update(){
+         System.out.println("Updated Successfully.. ");
+        return "Updated Successfully..";
+     }
+     @DeleteMapping("/delete")
+     public String remove(){
+         System.out.println("Deleted Successfully.. ");
+        return "Deleted successfully..";
+    }
 }
